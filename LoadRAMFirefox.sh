bd9aa1c1009a092c40f2a40379cf320d4a07bdb3
rchars <rchars@protonmail.com>
2024-05-19 17:48:14 +0200

    sec

--- LoadRAMFirefox.sh
+++ LoadRAMFirefox.sh
@@ -10,12 +10,14 @@
 mkdir -p /mnt/RAMDISK/RAMFirefox
 cp -r /usr/lib/firefox-esr /mnt/RAMDISK/RAMFirefox
 
+# TODO:
+# Fix the "firefox is running" problem
 cat <<'EOF' > /mnt/RAMDISK/RAMFirefox/RAMFirefox.sh
 #!/bin/bash
 FIREFOX_HOME=/mnt/RAMDISK/RAMFirefox/firefox-esr
 FIREFOX_PROFILE=~/.mozilla/firefox/1.default-esr
 MOZILLA_FIREFOX_PATH=$FIREFOX_HOME/firefox-esr
-sudo nice -n -20 sudo -u $USER $MOZILLA_FIREFOX_PATH -profile $FIREFOX_PROFILE & disown
+sudo nice -n -20 sudo -u $USER $MOZILLA_FIREFOX_PATH -profile $FIREFOX_PROFILE 1>/dev/null 2>/dev/null & disown
 EOF
 
 chmod u+x /mnt/RAMDISK/RAMFirefox/RAMFirefox.sh
